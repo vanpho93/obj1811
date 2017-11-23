16d40ac5193c4ded323b56ff8335eda7b3e42c80
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-23 19:40:05 +0700

    create new object

---
+++ a.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Teo',
+    age: 10
+};
+
+// const person = { name: 'Teo', age: 10 };
+
+console.log(person);
+console.log(person.name);
+person.age = 20;
+console.log(person);
